903dbb1535829563b9683d22b037e399450ae349
scarlettnik <swin-vu@ya.ru>
2025-03-11 12:40:14 +0300

    app like app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,76 @@
-import React, { useMemo, useRef } from 'react';
-import TinderCard from 'react-tinder-card';
+import React, { useState, useMemo, useRef } from 'react'
+import TinderCard from 'react-tinder-card'
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  setCurrentIndex,
-  setLastDirection,
-  addToDismatch,
-  addToMatch,
-  addToQueryMatch,
-  removeCard,
-} from '../features/cardSlice';
-import styles from './ui/card.module.css';
+import styles from './ui/card.module.css'
+
+
+const db = [
+  {
+    id: 1,
+    name: 'Dinesh Chugtai',
+    position: 'фронт',
+    skills: ['react', 'nextJS', 'brainfuck', 'strawberry'],
+    description: 'Издеваюсь над помидорами',
+    experience: '3 года',
+    links: ['https://qwerty.com', 'https://asdfgh.ru']
+  },
+  {
+    id: 2,
+    name: 'Dinesh Chugtai2',
+    description: 'Издеваюсь над помидорами',
+    skills: ['react', 'nextJS', 'brain'],
+    experience: '3 года',
+    links: ['https://qwerty.com', 'https://asdfgh.ru']
+  },
+  {
+    id: 3,
+    name: 'Dinesh Chugtai43',
+    position: 'фронт',
+    description: 'Издеваюсь над помидорами',
+    experience: '3 года',
+    links: ['https://qwerty.com', 'https://asdfgh.ru']
+  },
+  {
+    id: 4,
+    type: 'own',
+    name: 'Dinesh Chugtai4',
+    skills: ['react', 'nextJS', 'brainfuck', 'strawberry'],
+    position: 'бек',
+    description: 'Издеваюсь над помидорами',
+    experience: '3 года',
+    links: ['https://qwerty.com', 'https://asdfgh.ru']
+  },
+  {
+    type: 'team',
+    members: [
+      { name: 'x', position: 'фронт', experience: '3', skills:['1', '2'] },
+      { name: 'y', position: 'бек', experience: '3', skills:['2', '4'] },
+      { name: 'z', position: 'моб', experience: '3', skills:['3, 5'] }
+    ],
+    name: 'Dinesh Chugtai5',
+    description: 'Издеваюсь над помидорами, очень долго и мучительно изеваюсь над помидорами, совсем долго и мучительно издеваюсь над помидорами',
+    links: ['https://qwerty.com', 'https://asdfgh.ru']
+  }
+]
 
 function Card() {
-  const dispatch = useDispatch();
-  const { db, currentIndex, lastDirection } = useSelector((state) => state.card);
-
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const [lastDirection, setLastDirection] = useState();
   const currentIndexRef = useRef(currentIndex);
+  const [dismatch, setDismatch] = useState([]);
+  const [match, setMatch] = useState([]);
+  const [querymatch, setQueryMatch] = useState([]);
 
   const childRefs = useMemo(
       () =>
           Array(db.length)
               .fill(0)
               .map((i) => React.createRef()),
-      [db.length] // Зависимость от длины db
+      []
   );
 
   const updateCurrentIndex = (val) => {
-    dispatch(setCurrentIndex(val));
+    setCurrentIndex(val);
     currentIndexRef.current = val;
   };
 
@@ -36,17 +79,15 @@ function Card() {
   const canSwipe = currentIndex >= 0;
 
   const swiped = (direction, nameToDelete, index) => {
-    dispatch(setLastDirection(direction));
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
     const card = db[index];
-    dispatch(removeCard(card.id)); // Удаляем карточку из db
-    updateCurrentIndex(index - 1); // Обновляем currentIndex
-
     if (direction === 'left') {
-      dispatch(addToDismatch(card));
+      setDismatch((prevDismatch) => [...prevDismatch, card]);
     } else if (direction === 'right') {
-      dispatch(addToMatch(card));
+      setMatch((prevMatch) => [...prevMatch, card]);
     } else if (direction === 'down') {
-      dispatch(addToQueryMatch(card));
+      setQueryMatch((prevQueryMatch) => [...prevQueryMatch, card]);
     }
   };
 
@@ -74,22 +115,22 @@ function Card() {
           {db.map((character, index) => (
               <TinderCard
                   ref={childRefs[index]}
-                  key={character.id} // Используем id как ключ
+                  key={character.name}
                   className={styles.swipe}
                   onSwipe={(dir) => swiped(dir, character.name, index)}
                   onCardLeftScreen={() => outOfFrame(character.name, index)}
               >
                 <div className={styles.card}>
-                  <div style={{ display: 'flex' }}>
+                  <div style={{display: 'flex'}}>
                     <div>{character.name}</div>
                     <div className={styles.cardContent}>{character?.position}</div>
                     <div className={styles.cardContent}>{character?.experience}</div>
                   </div>
                   <div className={styles.achive}>
-                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
                       {character.skills &&
                           character.skills.map((item, index) => (
-                              <div style={{ margin: '1em' }} key={index}>
+                              <div style={{margin: '1em'}} key={index}>
                                 {item}
                               </div>
                           ))}
@@ -97,7 +138,7 @@ function Card() {
                   </div>
                   <div className={styles.cardContent}>Описание: {character.description || 'Данные не указаны'}</div>
                   <div>
-                    <Link to={`/product/${character.id}`} state={{ product: character }}>
+                    <Link to={`/product/${character.id}`}> {/* Используем id карточки */}
                       <button className={styles.linkStyle}>Подробнее</button>
                     </Link>
                   </div>

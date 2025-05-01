e5fa61885f54f92f8488fa8a83ae165bc2e8afe8
scarlettnik <marchuk.st@phystech.edu>
2025-05-01 16:21:26 +0300

    add prod fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { BackButton } from '@twa-dev/sdk/react';
 import ProductPage from './components/ProductPage';
 import TinderCards from "./components/TinderCards.jsx";
 import Profile from "./components/Profile.jsx";
@@ -22,57 +23,28 @@ function App() {
     );
 }
 
-
-const NavigationControls = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        if (!window.Telegram?.WebApp) return;
-
-        const isStartPage = location.pathname === '/'; // Укажите ваш путь стартовой страницы
-        const backButton = window.Telegram.WebApp.BackButton;
-
-        if (isStartPage) {
-            backButton.hide();
-            window.Telegram.WebApp.MainButton.show();
-            window.Telegram.WebApp.MainButton.setParams({
-                text: 'Закрыть',
-                color: '#FF3B30',
-            });
-        } else {
-            backButton.show();
-            window.Telegram.WebApp.MainButton.hide();
-        }
-
-        const handleClose = () => window.Telegram.WebApp.close();
-        window.Telegram.WebApp.MainButton.onClick(handleClose);
-
-        return () => {
-            window.Telegram.WebApp.MainButton.offClick(handleClose);
-        };
-    }, [location.pathname]);
-
-    return null;
-};
-
-
 function AppContent() {
     const navigate = useNavigate();
     const location = useLocation();
     const [showBackButton, setShowBackButton] = useState(false);
 
     useEffect(() => {
-        setShowBackButton(location.key !== 'default');
+        const isInitialPage = location.pathname === '/' && location.key === 'default';
+        setShowBackButton(!isInitialPage);
 
         if (window.Telegram?.WebApp?.BackButton) {
             window.Telegram.WebApp.BackButton.onClick(() => navigate(-1));
-            setShowBackButton(window.history.length > 1);
+            if (showBackButton) {
+                window.Telegram.WebApp.BackButton.show();
+            } else {
+                window.Telegram.WebApp.BackButton.hide();
+            }
         }
-    }, [location, navigate]);
+    }, [location, navigate, showBackButton]);
 
     return (
         <div>
-            <NavigationControls/>
+            {showBackButton && <BackButton onClick={() => navigate(-1)} />}
             <Routes>
                 <Route path="/" element={<TinderCards />} />
                 <Route path="/product/:id" element={<ProductPage />} />
